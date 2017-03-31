ca359c184cf1d24778c43c86e2e04dfac26719b2
elawson668 <elawsont668@gmail.com>
2017-03-31 13:41:28 -0400

    started lab 9

---
+++ Lab 9/lab9.md
@@ -0,0 +1,26 @@
+MongoDB setup - 
+
+![mongosetup](Photos/mongosetup.png)
+
+Find() - 
+
+![find](Photos/find.png)
+
+FindOne() - 
+
+![findone](Photos/findone.png)
+
+Find Word - 
+
+![findword](Photos/findword.png)
+
+Find ID - 
+
+![findid](Photos/findid.png)
+
+Word inserted and exported - 
+
+![wordinsert](Photos/wordinsert.png)
+
+
+
---
+++ Lab 9/mongo.py
@@ -0,0 +1,31 @@
+from pymongo import MongoClient
+import bson
+from bson.objectid import ObjectId
+
+client = MongoClient()
+db = client.csci2963
+defs = db.definitions
+
+print "All records"
+for post in defs.find():
+	print post
+
+print "\nOne record"
+doc = defs.find_one()
+print doc
+
+print "\nSpecific record"
+doc = defs.find_one({"word": "Capitaland"})
+print doc
+
+print "\nSpecific ID"
+def get(post_id):
+	document = defs.find_one({'_id': ObjectId(post_id)})
+	return document
+
+doc = get("56fe9e22bad6b23cde07b8ce")
+print doc
+
+print "\nInsert"
+post_id = defs.insert_one({"word": "Snufflepork", "definition": "grubblesnuff"})
+print post_id
